8c5bfc1718a0943a19a1f748ffb4bc55cba64a0c
codebysanjay <sanjaykakkatiri@gmail.com>
2020-12-07 23:49:59 +0530

    Added story feild

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Feed.css';
+import StoryReel from './StoryReel/StoryReel';
 
 function Feed() {
     return (
         <div className='feed'>
-            {/* Story Reel*/}
+            <StoryReel/>
             {/* Message Sender */}
         </div>
     )
